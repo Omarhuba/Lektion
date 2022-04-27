15259ace802ec38af415bd6134ed316d892863ba
Omarhuba <omaruba@gmail.com>
2022-04-27 09:47:04 +0200

    added new lektion and folder websocket

---
+++ Backend/WebSocket/websocket-chat-app/app.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const { Server} = require('socket.io')
+const http = require('http')
+
+const app = express()
+const server = http.createServer(app)
+const io = new Server(server)
+
+app.use(express.static('public'))
+
+
+io.use( (socket, next) =>{
+    console.log(socket.handshake);
+    if(socket.handshake.auth.tocken != 'hello'){
+        next(new Error('Unauthorizad'))
+    }else{
+        next()
+    }
+} )
+
+
+io.on('connection', (socket) =>{
+    console.log('Client connected with ID:' + socket.id);
+
+    socket.on('disconnect', () => {
+        console.log("Client disconnected");
+      })
+
+
+      socket.on('ping', message=>{
+          console.log('Recieved: ' +  message + 'from' + socket.id );
+          socket.broadcast.emit('pong', message)
+        })
+    })
+
+
+
+
+const PORT = 8080
+server.listen(PORT,()=>{
+    console.log(`SERVER STARTED ON PORT :${PORT}`);
+})
